4388ce8b06e9af8c8881ca5e80e91c6e663680ae
Oleksii Alybin <alybindev@gmail.com>
2022-12-08 14:40:14 +0200

    added logging

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,5 +1,6 @@
 const openai = require("../openai/config")
 const generateImage = async (req, res) => {
+    console.log("REQ MADE", req.body);
     const {prompt, size, count} = req.body;
     try {
         const response = await openai.createImage({
@@ -8,6 +9,7 @@ const generateImage = async (req, res) => {
             size: size === "small" ? "256x256" : size === "medium" ? "512x512" : "1024x1024"
         })
 
+        console.log("IMG RESP", response);
         const images = response.data?.data?.images
 
         res.status(200).json({images})
